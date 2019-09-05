d6127d397aba02fddca555385baa5d39e8793c1e
David <cichy.dawid@gmail.com>
2019-09-05 11:13:48 +0200

    Add PostgreSQLJDBC connection

--- src/main/java/com/pixel/dao/postgresql/PostgreSQLJDBC.java
+++ src/main/java/com/pixel/dao/postgresql/PostgreSQLJDBC.java
@@ -4,20 +4,19 @@ import java.sql.*;
 
 public class PostgreSQLJDBC {
     private Connection c;
-    private Statement stmt = null;
-    private ResultSet rs = null;
     private final String url;
     private String database;
     private String password;
 
     public PostgreSQLJDBC() {
-        this.url = "jdbc:postgresql://nabielec.synology.me:5432/postgres";
+
+        this.url = "jdbc:postgresql://nabielec.synology.me:5432/codecool?currentSchema=quest_store";
         this.database = "codecooler";
         this.password = "codecool";
         this.c = getConnection();
     }
 
-    private Connection getConnection() {
+    public Connection getConnection() {
         Connection c = null;
         try {
             c = DriverManager.getConnection(this.url, this.database, this.password); // set user and password
@@ -29,28 +28,12 @@ public class PostgreSQLJDBC {
         return c;
     }
 
-    public ResultSet executeQuery(String query) throws SQLException {
-        PreparedStatement ps = c.prepareStatement(query);
-        rs = ps.executeQuery();
-        return rs;
-    }
 
 
-
-    public void execute(String query) throws SQLException {
-        PreparedStatement ps = c.prepareStatement(query);
-        ps.executeUpdate();
-        c.close();
-    }
-
-    public void close(){
-        try {
-            c.close();
-            stmt.close();
-//            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+//    public void execute(String query) throws SQLException {
+//        PreparedStatement ps = c.prepareStatement(query);
+//        ps.executeUpdate();
+//        c.close();
+//    }
 
 }
