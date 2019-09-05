98e34d8a7f5ecafff90ce4aef69e2006a3b4232a
David <cichy.dawid@gmail.com>
2019-09-05 11:11:35 +0200

    Add UserDao

---
+++ src/main/java/com/pixel/dao/postgresql/UserDAO.java
@@ -0,0 +1,49 @@
+package com.pixel.dao.postgresql;
+
+import com.pixel.model.User;
+
+import java.sql.*;
+
+public class UserDAO {
+    private Connection c;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private Statement stmt;
+
+    public UserDAO() {
+        this.c = new PostgreSQLJDBC().getConnection();
+    }
+
+    public int getIdFromCredentials(String name, String password) throws SQLException {
+        String query = "SELECT * FROM users WHERE name = ? AND password = ? LIMIT 1";
+        this.ps = c.prepareStatement(query);
+        ps.setString(1, name);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            return rs.getInt("id");
+        }
+        return -1;
+    }
+
+    public boolean checkIfUserIsCreep(int id) throws SQLException {
+        String query = "SELECT * from creeps WHERE user_id = "+id+"";
+        this.stmt = c.createStatement();
+        this.rs = stmt.executeQuery(query);
+        return rs.next();
+    }
+
+    public boolean checkIfUserIsMentor(int id) throws SQLException {
+        String query = "SELECT * from mentors WHERE user_id = "+id+"";
+        this.stmt = c.createStatement();
+        this.rs = stmt.executeQuery(query);
+        return rs.next();
+    }
+
+    public boolean checkIfUserIsStudent(int id) throws SQLException {
+        String query = "SELECT * from students WHERE user_id = "+id+"";
+        this.stmt = c.createStatement();
+        this.rs = stmt.executeQuery(query);
+        return rs.next();
+    }
+}
