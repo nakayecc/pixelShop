2fff13f666d4fe48c6ecc536aca2c58275974d4d
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-09-18 15:24:41 +0200

    Add check user password when try to log in

--- src/main/java/com/pixel/controller/UserController.java
+++ src/main/java/com/pixel/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pixel.controller;
 
 import com.pixel.dao.postgresql.implementations.UserDAOI;
+
 import java.sql.SQLException;
 
 public class UserController {
@@ -12,15 +13,19 @@ public class UserController {
 
     public int getUserIdFromCredentials(String name, String password) throws SQLException {
         return userDAOI.getIdFromCredentials(name, password);
-        }
+    }
+
+    public boolean checkIfPasswordIsCorrect(String name, String password) throws SQLException {
+        return getUserIdFromCredentials(name, password) > 1;
+    }
 
     public String checkUserRank(int id) throws SQLException {
 
-        if (userDAOI.checkIfUserIsCreep(id)){
+        if (userDAOI.checkIfUserIsCreep(id)) {
             return "creep";
-        } else if (userDAOI.checkIfUserIsMentor(id)){
+        } else if (userDAOI.checkIfUserIsMentor(id)) {
             return "mentor";
-        } else if (userDAOI.checkIfUserIsStudent(id)){
+        } else if (userDAOI.checkIfUserIsStudent(id)) {
             return "student";
         }
         return null;
--- src/main/java/com/pixel/view/Login.java
+++ src/main/java/com/pixel/view/Login.java
@@ -1,18 +1,22 @@
 package com.pixel.view;
 
+import com.pixel.controller.UserController;
+import com.pixel.dao.postgresql.PostgreSQLJDBC;
 import com.pixel.dao.postgresql.implementations.SessionDAOI;
+import com.pixel.dao.postgresql.implementations.UserDAOI;
 import com.pixel.helper.Common;
-import com.pixel.model.Student;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpCookie;
-import java.net.URLDecoder;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,12 +26,16 @@ public class Login implements HttpHandler {
     private Common common;
 
     public Login() {
-        this.sessionDAOI = new SessionDAOI();
-        this.common = new Common();
+        sessionDAOI = new SessionDAOI();
+        common = new Common();
+
     }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        PostgreSQLJDBC postgreSQLJDBC = new PostgreSQLJDBC();
+        Connection connection = postgreSQLJDBC.getConnection();
+        UserController userController = new UserController(new UserDAOI(connection));
         CookieHandler cookieHandler = new CookieHandler();
         String response = "";
         String method = httpExchange.getRequestMethod();
@@ -38,7 +46,7 @@ public class Login implements HttpHandler {
         if (method.equals("GET")) {
             if (cookie.isPresent()) {
                 try {
-                    if (sessionDAOI.isCurrentSession(cookieHandler.extractCookieToString(cookie))){
+                    if (sessionDAOI.isCurrentSession(cookieHandler.extractCookieToString(cookie))) {
                         httpExchange.getResponseHeaders().set("Location", "/");
                         httpExchange.sendResponseHeaders(303, response.getBytes().length);
                     } else {
@@ -53,31 +61,43 @@ public class Login implements HttpHandler {
         }
 
 
-        // If the form was submitted, retrieve it's content.
         if (method.equals("POST")) {
-            //todo check user and password, create new cookie, insert it to db, redirect to
 
 
-            if (cookie != null) {
-                InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
-                BufferedReader br = new BufferedReader(isr);
-                String formData = br.readLine();
-                Map inputs = common.parseFormData(formData);
-                //todo check password and username
-                cookies = new HttpCookie("sessionId", generateSessionID());
-                httpExchange.getResponseHeaders().add("Set-Cookie", cookies.toString());
-                try {
-                    sessionDAOI.createSession(cookies.getValue(), 8);
-                    System.out.println("create session");
-                } catch (SQLException e) {
-                    e.printStackTrace();
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+            Map inputs = common.parseFormData(formData);
+            String username = String.valueOf(inputs.get("username"));
+            String password = String.valueOf(inputs.get("password"));
+            try {
+                if (userController.checkIfPasswordIsCorrect(username, password)) {
+                    cookies = new HttpCookie("sessionId", generateSessionID());
+                    httpExchange.getResponseHeaders().add("Set-Cookie", cookies.toString());
+                    try {
+                        int userId = userController.getUserIdFromCredentials(username, password);
+                        sessionDAOI.createSession(cookies.getValue(), userId);
+                        System.out.println("create session");
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    httpExchange.getResponseHeaders().set("Location", "/");
+                    httpExchange.sendResponseHeaders(303, response.getBytes().length);
+                } else {
+                    System.out.println("bad password");
+                    response = common.getLoginTemplate();
                 }
-                //response = loginUser(httpExchange);
-                httpExchange.getResponseHeaders().set("Location", "/");
-                //redirectToUserLandPage(httpExchange, userId);
-                httpExchange.sendResponseHeaders(303, response.getBytes().length);
-            } else {
-                response = common.getLoginTemplate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
             }
 
         }
@@ -93,9 +113,6 @@ public class Login implements HttpHandler {
         UUID generatedId = UUID.randomUUID();
         return generatedId.toString();
     }
-
-
-
 
 
     private String loginUser(HttpExchange httpExchange) throws IOException {
